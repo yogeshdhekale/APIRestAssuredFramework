f8028ae504a518e5b7efbe8ba2b331fc80c59a08
yogeshdhekale <dhekale.yogesh@gmail.com>
2022-01-31 21:56:44 +0530

    Rest Assured Framework Post through Excel Data Provider

--- src/main/java/com/qa/api/Gorest/util/ExcelUtil.java
+++ src/main/java/com/qa/api/Gorest/util/ExcelUtil.java
@@ -1,5 +1,55 @@
 package com.qa.api.Gorest.util;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
 public class ExcelUtil {
+
+	public static Workbook book;
+	public static Sheet sheet;
+
+	public static String TestDataSheetPath = "/Users/yogeshdhekale/eclipse-workspace/APIRestAssuredFramework/"
+			+ "src/main/java/com/qa/api/Gorest/TestData/GoRestTestData.xlsx";
+
+	public static Object[][] getTestDataExcel(String sheetName) {
+
+		try {
+			FileInputStream input = new FileInputStream(TestDataSheetPath);
+			try {
+				try {
+					book = WorkbookFactory.create(input);
+					sheet = book.getSheet(sheetName);
+				} catch (InvalidFormatException e) {
+					e.printStackTrace();
+				}
+
+			} catch (EncryptedDocumentException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		int rowCount = sheet.getLastRowNum();
+		int colCount = sheet.getRow(0).getLastCellNum();
+		Object data[][] = new Object[rowCount][colCount];
+
+		for (int i = 0; i < rowCount; i++) {
+			for (int j = 0; j < colCount; j++) {
+				data[i][j] = sheet.getRow(i + 1).getCell(j).toString();
+
+			}
+		}
+		return data;
+	}
 
 }
--- src/test/java/com/qa/api/Gorest/tests/CreateUserPostTest.java
+++ src/test/java/com/qa/api/Gorest/tests/CreateUserPostTest.java
@@ -1,9 +1,13 @@
 package com.qa.api.Gorest.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.qa.api.Gorest.RestClient.RestClient;
 import com.qa.api.Gorest.pojo.User;
+import com.qa.api.Gorest.util.ExcelUtil;
+
+import io.restassured.response.Response;
 
 public class CreateUserPostTest {
 	
@@ -11,12 +15,38 @@ public class CreateUserPostTest {
 	String basePath = "/public-api/users";
 	String token = "cb5d7e66b3c6c8d780f3056c01fe509d97f2b67d37b04cc465e62e339577b7f2";
 	
+	@DataProvider
+	public Object[][] getUserData() {
+		Object userData[][] = ExcelUtil.getTestDataExcel("userData");
+		return userData;
+	}
+	
+	
+	@Test(dataProvider = "getUserData")
+	public void createUserExcelAPIPostTest(String name, String email, String gender, String status) {
+		
+	//	User user = new User("Suryakant Mishra", "surya.mishra@hotmail.com", "male", "active");
+		
+		User user = new User(name, email, gender, status);
+		
+		Response response = RestClient.doPost("JSON", baseURI, basePath, token, null, user, true);
+		
+		System.out.println(response.prettyPrint());
+		System.out.println("*************************************");
+		System.out.println(response.statusCode());
+		
+	}
+	
+	
 	@Test
 	public void createUserAPIPostTest() {
 		
 		User user = new User("Suryakant Mishra", "surya.mishra@hotmail.com", "male", "active");
 		
-		RestClient.doPost("JSON", baseURI, basePath, token, null, user, true);
+		Response response = RestClient.doPost("JSON", baseURI, basePath, token, null, user, true);
+		
+		System.out.println(response.prettyPrint());
+		System.out.println(response.statusCode());
 		
 	}
 
